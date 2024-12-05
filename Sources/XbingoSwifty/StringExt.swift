1ab6e2be1937ba03795233be4663e22ba4e62f59
xbingo <dove025@qq.com>
2024-12-05 19:21:05 +0800

    feat: 增加字体计算

--- Sources/XbingoSwifty/StringExt.swift
+++ Sources/XbingoSwifty/StringExt.swift
@@ -5,6 +5,7 @@
 //  Created by xbingo on 2024/12/4.
 //
 
+import UIKit
 
 // 手机号码相关
 extension String {
@@ -18,5 +19,52 @@ extension String {
     public var desensitizedText: String {
         let regex = "(\\d{3})(\\d{4})(\\d{4})"
         return self.replacingOccurrences(of: regex, with: "$1****$3", options: .regularExpression)
+    }
+}
+
+// 高度计算
+extension String {
+    
+    public func boundingRect(with constrainedSize: CGSize, font: UIFont, lineSpacing: CGFloat? = nil) -> CGSize {
+        let attritube = NSMutableAttributedString(string: self)
+        let range = NSRange(location: 0, length: attritube.length)
+        attritube.addAttributes([NSAttributedString.Key.font: font], range: range)
+        if lineSpacing != nil {
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.lineSpacing = lineSpacing!
+            attritube.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: range)
+        }
+        
+        let rect = attritube.boundingRect(with: constrainedSize, options: [.usesLineFragmentOrigin, .usesFontLeading], context: nil)
+        var size = rect.size
+        
+        if let currentLineSpacing = lineSpacing {
+            // 文本的高度减去字体高度小于等于行间距，判断为当前只有1行
+            let spacing = size.height - font.lineHeight
+            if spacing <= currentLineSpacing && spacing > 0 {
+                size = CGSize(width: size.width, height: font.lineHeight)
+            }
+        }
+        
+        return size
+    }
+    
+    public func boundingRect(with constrainedSize: CGSize, font: UIFont, lineSpacing: CGFloat? = nil, lines: Int) -> CGSize {
+        if lines < 0 {
+            return .zero
+        }
+        
+        let size = boundingRect(with: constrainedSize, font: font, lineSpacing: lineSpacing)
+        if lines == 0 {
+            return size
+        }
+        
+        let currentLineSpacing = (lineSpacing == nil) ? (font.lineHeight - font.pointSize) : lineSpacing!
+        let maximumHeight = font.lineHeight*CGFloat(lines) + currentLineSpacing*CGFloat(lines - 1)
+        if size.height >= maximumHeight {
+            return CGSize(width: size.width, height: maximumHeight)
+        }
+        
+        return size
     }
 }
